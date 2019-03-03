4bfc06bc64d6e9daf8d335319e1efb7bffc75f1a
Emmanuel Porfirio <emmanuel@omnigecko.com>
2019-03-03 07:15:56 -0500

    All Pages Styles Completed

--- page-tips.php
+++ page-tips.php
@@ -5,9 +5,7 @@
  *
  * @package garbage-geek
  */
-
 get_header();
-
 $cat = isset( $_GET['tip-category'] ) ? $_GET['tip-category'] : false;
 if( $cat ) {
 	$query_obj = array( 
@@ -20,7 +18,6 @@ if( $cat ) {
 		'post_type' => 'geek_tip'
 	);
 }
-
 query_posts( $query_obj );
 ?>
 	<div id="primary" class="content-area">
@@ -44,25 +41,52 @@ query_posts( $query_obj );
 				margin-top: 12vw;
 			}
 			#main form Select{
+				display: block;
 				width: 80vw;
 				height: 6vw;
 				margin: auto;
 				margin-top: 17vw;
 			}
+			#main .slider{
+				min-height: 80vh;
+			}
+			#main .slider .slide{
+				background: #2D303E !important;
+				text-align: center;
+				font-size: 4vw;
+				line-height: 8vw;
+				color: #FFF;
+				padding-top: 2vw;
+				padding-bottom: 2vw;
+				padding-right: 1em;
+				margin-top: 10vw;
+			}
+			#main .slider .slide h2{
+				display: block;
+				height: 24vw;
+			}
+			#main .slider .slide span{
+				display: block;
+				font-size: 5vw;
+				line-height: 10vw;
+				font-weight:500;
+				text-align: center;
+				color: #93C840;
+				margin-bottom: 3vw;
+    		}
 		</style>
-		<?php
-		if( have_posts() ):
-			while ( have_posts() ) :
-				the_post();
-				get_template_part('template-parts/content', get_post_type());
-			endwhile; // End of the loop.
-		else:
-		?>
-			<p>No tips today...</p>
-		<?php
-		endif;
-		?>
-
+		<div class='slider'>
+			<?php
+				if( have_posts() ):
+					while ( have_posts() ) :
+						the_post();
+						get_template_part('template-parts/content', get_post_type());
+					endwhile; // End of the loop.
+				else:?>
+					<p>No tips today...</p><?php
+				endif;
+			?>
+		</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
